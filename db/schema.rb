972f638e93490286216f12ec7a49a945bf46ce10
Finnegan Hewitt <fbhewitt@gmail.com>
2015-03-05 15:18:47 -0700

    fixed user creation with default role

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150305181220) do
+ActiveRecord::Schema.define(version: 20150305221551) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 20150305181220) do
     t.string "last_name"
     t.string "email"
     t.string "password_digest"
-    t.string "role"
+    t.string "role",            default: "member"
   end
 
 end
