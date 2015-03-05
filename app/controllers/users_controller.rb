5650f500b6203c6df9a9cec29d5d40fde817ab50
Finnegan Hewitt <fbhewitt@gmail.com>
2015-03-05 15:33:21 -0700

    fixed create action for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,6 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      end
       session[:user_id] = @user.id
       redirect_to root_path, notice: "You have successfully signed up."
     else
